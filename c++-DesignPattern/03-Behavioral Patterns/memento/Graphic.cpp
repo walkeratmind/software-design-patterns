edc99a3bc87074488535a9b0956d4afe276df398
Rakesh Niraula <rakeshhniraulaa@gmail.com>
2020-06-10 08:51:15 +0545

    add chain of responsibility pattern

---
+++ c++-DesignPattern/03-Behavioral Patterns/memento/Graphic.cpp
@@ -0,0 +1,18 @@
+#include "iostream"
+
+
+/*
+------------------------------------------------------------
+- title: Graphic.cpp
+- Author: Rakesh Niraula
+- copyright (C) 2020. All rights reserved
+- date: 2020-05-21 13:29:07
+------------------------------------------------------------
+*/
+
+class Graphic;
+
+int main() {
+
+    return 0;
+}
